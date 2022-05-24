6f2a6c969afe83c78d7b15f6e72b8cd128ab76d7
윤수환 <suhwan0824@naver.com>
2022-05-24 17:24:29 +0900

    Spring_Inflearn

---
+++ src/test/java/com/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,27 @@
+package com.hello.core.member;
+
+import com.hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
